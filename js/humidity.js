27e8c5cdeaa37a66a467ff0fd676214aeb0c0fd5
IlariaStocchi <ilaria.stocchi14@gmail.com>
2021-08-16 16:20:00 +0200

    refactor: one method to build all humidity graph + zooming feature

--- js/humidity.js
+++ js/humidity.js
@@ -1,6 +1,6 @@
-var margin = {top: 200, right: 20, bottom: 20, left: 450},
-    width = 1100,
-    height = 200,
+var margin = {top: 200, right: 20, bottom: 20, left: 40}, //200, 20, 20, 40
+    width = 1100,  //1100
+    height = 200,  //200
     screenWidth = 1800,
     screenHeight = 900,
     screenMargin1 = 770,
@@ -26,199 +26,93 @@ function createSVG(){
     return svg;
 } 
     
-function humidityJuly() {
+function humidityGraph(month) {
         
     var svg = createSVG();
-                
-    d3.csv("data/CSV/07/2020-07-humidity-average.csv", function(d){ return { date : d3.timeParse("%Y-%m-%dT%H:%M:%S.%LZ")(d.date), value : d.value }})
-    .then(function(data) {
-        const x = d3.scaleTime()
-        .domain(d3.extent(data, function(d) { return d.date; }))
-        .range([ 0, 500 ]);
-                    
-        svg.append("g")
-        .attr("transform", `translate(0, 200)`)
-        .call(d3.axisBottom(x));
-                    
-        const y = d3.scaleLinear()
-        .domain([0, d3.max(data, function(d) { return +d.value; })])
-        .range([ (screenHeight - screenMargin2), 0 ]);
-                    
-        svg.append("g")
-        .call(d3.axisLeft(y));
-                   
-        svg.append("path")
-        .datum(data)
-        .attr("fill", "none")
-        .attr("stroke", "blue")
-        .attr("stroke-width", 1.5)
-        .attr("d", d3.line()
-        .x(function(d) { return x(d.date) })
-        .y(function(d) { return y(d.value) })
-        )
-    })    
-}
+    var path = "data/CSV/";
+    var month = month.concat("/2020-").concat(month);
+    var sensor = "-humidity-average.csv";
+    var file = path.concat(month).concat(sensor);
     
-    
-function humidityAugust() {
-        
-    var svg = createSVG();
-                
-    d3.csv("data/CSV/08/2020-08-humidity-average.csv", function(d){ return { date : d3.timeParse("%Y-%m-%dT%H:%M:%S.%LZ")(d.created), value : d.value }})
+    d3.csv(file, function(d){ return { date : d3.timeParse("%Y-%m-%dT%H:%M:%S.%LZ")(d.created), value : d.value }})
     .then(function(data) {
         const x = d3.scaleTime()
-        .domain(d3.extent(data, function(d) { return d.date; }))
-        .range([ 0, 500 ]);
+            .domain(d3.extent(data, function(d) { return d.date; }))
+            .range([ 0, width ]);  //500
                     
-        svg.append("g")
-        .attr("transform", `translate(0, 200)`)
-        .call(d3.axisBottom(x));
-                    
-        const y = d3.scaleLinear()
-        .domain([0, d3.max(data, function(d) { return +d.value; })])
-        .range([ (screenHeight - screenMargin2), 0 ]);
-                    
-        svg.append("g")
-        .call(d3.axisLeft(y));
-                   
-        svg.append("path")
-        .datum(data)
-        .attr("fill", "none")
-        .attr("stroke", "blue")
-        .attr("stroke-width", 1.5)
-        .attr("d", d3.line()
-        .x(function(d) { return x(d.date) })
-        .y(function(d) { return y(d.value) })
-        )
-    })    
-}
-
-function humiditySeptember() {
-        
-    var svg = createSVG();
-                
-    d3.csv("data/CSV/09/2020-09-humidity-average.csv", function(d){ return { date : d3.timeParse("%Y-%m-%dT%H:%M:%S.%LZ")(d.created), value : d.value }})
-    .then(function(data) {
-        const x = d3.scaleTime()
-        .domain(d3.extent(data, function(d) { return d.date; }))
-        .range([ 0, 500 ]);
-                    
-        svg.append("g")
-        .attr("transform", `translate(0, 200)`)
-        .call(d3.axisBottom(x));
-                    
-        const y = d3.scaleLinear()
-        .domain([0, d3.max(data, function(d) { return +d.value; })])
-        .range([ (screenHeight - screenMargin2), 0 ]);
-                    
-        svg.append("g")
-        .call(d3.axisLeft(y));
-                   
-        svg.append("path")
-        .datum(data)
-        .attr("fill", "none")
-        .attr("stroke", "blue")
-        .attr("stroke-width", 1.5)
-        .attr("d", d3.line()
-        .x(function(d) { return x(d.date) })
-        .y(function(d) { return y(d.value) })
-        )
-    })    
-}
-
-function humidityOctober() {
-    var svg = createSVG();
-    d3.csv("data/CSV/10/2020-10-humidity-average.csv", function(d){ return { date : d3.timeParse("%Y-%m-%dT%H:%M:%S.%LZ")(d.created), value : d.value }})
-    .then(function(data) {
-        const x = d3.scaleTime()
-        .domain(d3.extent(data, function(d) { return d.date; }))
-        .range([ 0, 500 ]);
-            
-        svg.append("g")
-        .attr("transform", `translate(0, 200)`)
-        .call(d3.axisBottom(x));
-            
-        const y = d3.scaleLinear()
-        .domain([0, d3.max(data, function(d) { return +d.value; })])
-        .range([ (screenHeight - screenMargin2), 0 ]);
-            
-        svg.append("g")
-        .call(d3.axisLeft(y));
-                    
-        svg.append("path")
-        .datum(data)
-        .attr("fill", "none")
-        .attr("stroke", "blue")
-        .attr("stroke-width", 1.5)
-        .attr("d", d3.line()
-        .x(function(d) { return x(d.date) })
-        .y(function(d) { return y(d.value) })
-        )
-    })
-}
-
-
-function humidityNovember() {
-    var svg = createSVG();
-    d3.csv("data/CSV/11/2020-11-humidity-average.csv", function(d){ return { date : d3.timeParse("%Y-%m-%dT%H:%M:%S.%LZ")(d.created), value : d.value }})
-    .then(function(data) {
-        const x = d3.scaleTime()
-        .domain(d3.extent(data, function(d) { return d.date; }))
-        .range([ 0, 500 ]);
-            
-            svg.append("g")
+        xAxis = svg.append("g")
             .attr("transform", `translate(0, 200)`)
             .call(d3.axisBottom(x));
-            
-            const y = d3.scaleLinear()
+                    
+        const y = d3.scaleLinear()
             .domain([0, d3.max(data, function(d) { return +d.value; })])
             .range([ (screenHeight - screenMargin2), 0 ]);
-            
-            svg.append("g")
+                    
+        yAxis = svg.append("g")
             .call(d3.axisLeft(y));
-                    
-            svg.append("path")
+        
+        const clip = svg.append("defs").append("svg:clipPath")
+            .attr("id", "clip")
+            .append("svg:rect")
+            .attr("width", width)   //width
+            .attr("height", height)  //height  quanto vedo di grafico all'inizio
+            .attr("x", 0)
+            .attr("y", 0);
+        
+        const brush = d3.brushX() 
+            .extent([[0,0], [width, height]])  //width, height   area selezione
+            .on("end", updateChart)
+        
+        const line = svg.append("g")
+            .attr("clip-path", "url(#clip)")
+            
+        line.append("path")
             .datum(data)
+            .attr("class", "line")
             .attr("fill", "none")
-            .attr("stroke", "blue")
-            .attr("stroke-width", 1.5)
+            .attr("stroke", "steelblue")
             .attr("d", d3.line()
-            .x(function(d) { return x(d.date) })
-            .y(function(d) { return y(d.value) })
-            )
-        })
+                .x(function(d) {return x(d.date)})
+                .y(function(d) {return y(d.value)})
+             )
+        line.append("g")
+            .attr("class", "brush")
+            .call(brush)
+            
+        let idleTimeout
+        function idled() {idleTimeout = null; }
+        
+        function updateChart(event,d){
+            extent = event.selection
+            
+            if(!extent){
+                if (!idleTimeout) return idleTimeout = setTimeout(idled, 350);
+                x.domain([ 4,8])
+            } else {
+                x.domain([ x.invert(extent[0]), x.invert(extent[1]) ])
+                line.select(".brush").call(brush.move, null)
+            }
+            xAxis.transition().duration(1000).call(d3.axisBottom(x))
+            line.select('.line')
+                .transition()
+                .duration(1000)
+                .attr("d", d3.line()
+                    .x(function(d) {return x(d.date) })
+                    .y(function(d) {return y(d.value) }))
+        }
+        
+        svg.on("dblclick", function(){
+            x.domain(d3.extent(data, function(d) {return d.date; }))
+            xAxis.transition().call(d3.axisBottom(x))
+            line.select('.line')
+                .transition()
+                .attr("d", d3.line()
+                    .x(function(d) {return x(d.date) })
+                    .y(function(d) {return y(d.value) }))
+        });
+ })
 }
-
-function humidityDecember() {
-    var svg = createSVG();
-    d3.csv("data/CSV/12/2020-12-humidity-average.csv", function(d){ return { date : d3.timeParse("%Y-%m-%dT%H:%M:%S.%LZ")(d.created), value : d.value }})
-    .then(function(data) {
-        const x = d3.scaleTime()
-        .domain(d3.extent(data, function(d) { return d.date; }))
-        .range([ 0, 500 ]);
-            
-        svg.append("g")
-        .attr("transform", `translate(0, 200)`)
-        .call(d3.axisBottom(x));
-            
-        const y = d3.scaleLinear()
-        .domain([0, d3.max(data, function(d) { return +d.value; })])
-        .range([ (screenHeight - screenMargin2), 0 ]);
-            
-        svg.append("g")
-        .call(d3.axisLeft(y));
-                    
-        svg.append("path")
-        .datum(data)
-        .attr("fill", "none")
-        .attr("stroke", "blue")
-        .attr("stroke-width", 1.5)
-        .attr("d", d3.line()
-        .x(function(d) { return x(d.date) })
-        .y(function(d) { return y(d.value) })
-        )
-    })
-}
+    
     
 function buildHumidityGraph() {
         
@@ -226,23 +120,27 @@ function buildHumidityGraph() {
     
     switch(option){
         case "july":
-            humidityJuly();
+            humidityGraph("07");
             break;
             
         case "august":
-            humidityAugust();
+            humidityGraph("08");
             break;
             
         case "september":
-            humiditySeptember();
+            humidityGraph("09");
             break;
         
         case "october":
-            humidityOctober();
+            humidityOctober("10");
             break;
         
         case "november":
-            humidityDecember();
+            humidityGraph("11");
+            break;
+        
+        case "november":
+            humidityDecember("12");
             break;
     }
 }
