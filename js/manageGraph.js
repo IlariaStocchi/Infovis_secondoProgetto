434faf6f13e502dd95b7a1f19a731bca8408673c
IlariaStocchi <ilaria.stocchi14@gmail.com>
2021-08-23 16:05:00 +0200

    fixed second graph position

--- js/manageGraph.js
+++ js/manageGraph.js
@@ -42,7 +42,8 @@ function createSecondSVG(){
     .attr("width", width + margin.left + margin.right)
     .attr("height", height + margin.top + margin.bottom)
     .append("g")
-    .attr("transform", `translate(${margin.left},${margin.top})`);
+    //.attr("transform", `translate(${margin.left},${margin.top})`);
+    .attr("transform", `translate(${margin.left},${(margin.top)-120})`);
     return svg;
 }  
 
